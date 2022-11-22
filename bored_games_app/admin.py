a1f927b811b922a187bc99e50dddf380a9cc06a1
Goo <110427078+arekune@users.noreply.github.com>
2022-11-22 21:13:16 +0200

    Register models in admin

--- bored_games_app/admin.py
+++ bored_games_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class BoardGameAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BoardGame, BoardGameAdmin)
+
+class BoardGameInstanceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BoardGameInstance, BoardGameInstanceAdmin)
+
+class BoardGameRentalAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BoardGameRental, BoardGameRentalAdmin)
+
+class BoardGameReviewAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BoardGameReview, BoardGameReviewAdmin)
