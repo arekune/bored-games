db155dba75be740701a1f174f6108f4e819ef4f6
Goo <110427078+arekune@users.noreply.github.com>
2022-11-22 21:22:16 +0200

    Add landing page view function, class-based view for game details

--- bored_games_app/views.py
+++ bored_games_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from . import models
 
 # Create your views here.
+
+# VIEW FUNCTIONS
+
+def landing_page_view(request):
+    return render(request, "bored_games_app/index.html")
+
+# CLASS-BASED VIEWS
+
+class GameDetail(DetailView):
+
+    model = models.BoardGame
