93c34917149844a83f8c82c9fc9192322d44a397
Goo <110427078+arekune@users.noreply.github.com>
2022-12-09 12:18:19 +0200

    Admin dashboard finished

--- bored_games_app/admin.py
+++ bored_games_app/admin.py
@@ -4,16 +4,29 @@ from .models import *
 # Register your models here.
 
 class BoardGameAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = ("id", "title", "date_added", "date_modified")
+    list_display_links = ("id", "title")
+    ordering = ("id", "title")
+
 
 class BoardGameInstanceAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = ("id", "game", "status", "latest_borrower", "date_modified", "date_added")
+    list_display_links = ("id", "game")
+    ordering = ("id", "game", "status", "latest_borrower", "date_modified")
 
 class BoardGameRentalAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ("id", "game_instance", "borrower", "date_rented")
+    list_display_links = ("id", "game_instance")
+    ordering = ("id", "game_instance", "borrower", "date_rented")
 
 class BoardGameReviewAdmin(admin.ModelAdmin):
-    pass
+    
+    list_display = ("id", "game", "review_author", "date_written", "date_edited")
+    list_display_links = ("id", "game", "review_author")
+    ordering = ("id", "game", "review_author", "date_written", "date_edited")
 
 admin.site.register(BoardGame, BoardGameAdmin)
 admin.site.register(BoardGameInstance, BoardGameInstanceAdmin)
